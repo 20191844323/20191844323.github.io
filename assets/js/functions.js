7883de1f5a2eaca48cf14755827dea6ffcd37745
20191844323 <3080300351@qq.com>
2024-07-21 14:55:06 +0100

    Update 2024.07.21

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,3 +5,25 @@ jQuery( document ).ready(function() {
     });
     
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Bootstrap 的下拉菜单在悬停时打开
+    var dropdowns = document.querySelectorAll('.nav-item.dropdown');
+    dropdowns.forEach(function(dropdown) {
+        dropdown.addEventListener('mouseenter', function() {
+            var dropdownMenu = this.querySelector('.dropdown-menu');
+            if (dropdownMenu) {
+                dropdown.classList.add('show');
+                dropdownMenu.classList.add('show');
+            }
+        });
+        dropdown.addEventListener('mouseleave', function() {
+            var dropdownMenu = this.querySelector('.dropdown-menu');
+            if (dropdownMenu) {
+                dropdown.classList.remove('show');
+                dropdownMenu.classList.remove('show');
+            }
+        });
+    });
+});
+
